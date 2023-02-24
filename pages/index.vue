28d0cd36da95ad9c9b35627f0e59478269075e7e
Harikrishnan <hknp796@gmail.com>
2023-02-24 14:38:16 +0530

    footer added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <Why />
+  <Footer />
 </template>
 
 <script setup>
-import Why from "../components/static/Why.vue";
+// import Why from "../components/static/Why.vue";
+import Footer from "../components/static/Footer.vue";
 </script>
