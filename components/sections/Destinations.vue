c841cb554bb97a017c1bc5dd6bd7d3d2443e99b3
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-02-25 16:03:08 +0530

    added layout for section

--- components/sections/Destinations.vue
+++ components/sections/Destinations.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4">
-    <BaseRedirectTitle title="Top Destination" to="#" />
+    <BaseRedirectTitle title="Top Destination" to="/top-destination" />
 
     <div class="grid gap-2 rounded-xl mt-4">
       <div class="col-span-2"><CardImage /></div>
---
+++ layouts/Empty.vue
@@ -0,0 +1,9 @@
+<template>
+  <main
+    class="w-full mobile-container min-h-screen flex flex-col justify-between"
+  >
+    <slot></slot>
+  </main>
+</template>
+
+<script setup></script>
