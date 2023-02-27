66b9bfe6f5c93625dc786521534428a5f0b29402
Harikrishnan <hknp796@gmail.com>
2023-02-27 20:30:25 +0530

    tabs added in search section

---
+++ components/core/Tabs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-full py-2 sm:px-0">
+    <TabGroup>
+      <TabList class="flex">
+        <Tab
+          v-for="category in Object.keys(categories)"
+          as="template"
+          :key="category"
+          v-slot="{ selected }"
+        >
+          <button
+            :class="[
+              'w-full  py-2.5 text-sm font-medium ',
+              selected
+                ? 'bg-white  text-[#70C3D4] border-b-2 border-[#70C3D4]  focus:bg-white focus:outline-none'
+                : 'text-[#70C3D4] hover:bg-white/[0.12] ',
+            ]"
+          >
+            {{ category }}
+          </button>
+        </Tab>
+      </TabList>
+
+      <TabPanels class="mt-2">
+        <TabPanel>
+          <SectionsSearch />
+        </TabPanel>
+      </TabPanels>
+    </TabGroup>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
+
+const categories = ref({
+  Flight: [],
+  Activites: [],
+  More: [],
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-icon", "@pinia/nuxt", " nuxt-headlessui"],
+  modules: ["nuxt-icon", "@pinia/nuxt", "nuxt-headlessui"],
   css: ["~/assets/css/main.css"],
 
   postcss: {
