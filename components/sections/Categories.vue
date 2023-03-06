fb0a5d018ec590cfdefe85721a210ae36185c39f
Harikrishnan <hknp796@gmail.com>
2023-03-06 17:07:59 +0530

    scroll button added in sections

--- components/sections/Categories.vue
+++ components/sections/Categories.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="p-4">
+  <div class="p-4 relative">
     <BaseRedirectTitle title="Top Categories" to="/top-categories" />
-    <div class="flex overflow-x-scroll mt-4 gap-5 lg:w-full">
+    <div
+      class="flex overflow-x-scroll mt-4 gap-5 lg:w-full"
+      ref="scrollSection"
+    >
       <CardInfo
         v-bind="test"
         class="lg:min-w-[430px] lg:min-h-[325px]"
@@ -9,6 +12,13 @@
         :key="index"
       />
       <CardInfo v-bind="test2" class="lg:min-w-[430px] lg:min-h-[325px]" />
+      <button
+        type="button"
+        class="text-black bg-white border hover:bg-gray-200 font-medium rounded-full text-sm p-2.5 text-center items-center absolute -right-3 top-[200px] hidden lg:block"
+        @click="scrolling"
+      >
+        <Icon name="mdi-light:arrow-right" width="30" height="30" />
+      </button>
     </div>
   </div>
 </template>
@@ -30,4 +40,12 @@ const test2 = {
   image: "/png/Flight.png",
   alt: "",
 };
+
+const scrollSection = ref(null);
+function scrolling() {
+  scrollSection.value.scrollBy({
+    left: 900,
+    behavior: "smooth",
+  });
+}
 </script>
