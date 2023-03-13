d98d1b9222788235a1f56c9b2175424641c81d0c
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-13 20:52:03 +0530

    fixed search value, infinite loading

--- components/button/Primary.vue
+++ components/button/Primary.vue
@@ -2,7 +2,7 @@
   <button
     :disabled="isLoading"
     :type="type"
-    @click="type != 'submit' ? handleClick : ''"
+    @click="type !== 'submit' ? handleClick() : ''"
     class="text-black focus:ring-4 outline outline-gray-300 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center"
   >
     <div v-if="isLoading">
