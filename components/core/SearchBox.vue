fbea9d7d61b970cb940c850bce2742db62320e54
Harikrishnan <harikrishnan@Harikrishnans-MacBook-Air.local>
2023-03-14 12:50:35 +0530

    style updates in search

--- components/core/SearchBox.vue
+++ components/core/SearchBox.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="w-full flex flex-col items-center justify-center gap-7">
-    <div class="relative flex justify-center items-center w-full">
-      <div
-        class="absolute inset-y-0 flex items-center pl-3 pointer-events-none w-[50%]"
-      >
-        <Icon name="material-symbols:search-sharp" size="20" />
+    <div class="flex gap-4">
+      <div class="relative flex-1">
+        <div
+          class="absolute inset-y-0 flex items-center pl-3 pointer-events-none"
+        >
+          <Icon name="material-symbols:search-sharp" size="20" />
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          v-model="keyword"
+          class="p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none flex-1 w-96"
+          placeholder="Search"
+          required
+        />
       </div>
-      <input
-        type="search"
-        id="default-search"
-        v-model="keyword"
-        class="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none w-[50%]"
-        placeholder="Search"
-        required
-      />
-    </div>
 
-    <select
-      id="countries"
-      v-model="selected"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-    >
-      <option>Choose a country</option>
-      <option
-        v-for="country in countries"
-        :key="country.countryId"
-        :value="country.countryId"
+      <select
+        id="countries"
+        v-model="selected"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
       >
-        {{ country.countryName }}
-      </option>
-    </select>
+        <option>Choose a country</option>
+        <option
+          v-for="country in countries"
+          :key="country.countryId"
+          :value="country.countryId"
+        >
+          {{ country.countryName }}
+        </option>
+      </select>
+    </div>
     <ButtonPrimary type="button" :is-loading="loading" @click="search"
       >Search</ButtonPrimary
     >
