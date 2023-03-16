a432d025738aa64754d713876c6c5541d21ab344
Harikrishnan <harikrishnan@Harikrishnans-MacBook-Air.local>
2023-03-16 22:27:44 +0530

    subtitle added in initial page load

--- composables/initalPageLoad.js
+++ composables/initalPageLoad.js
@@ -11,31 +11,37 @@ export const useStatic = defineStore("staticData", () => {
       cityId: 13160,
       cityName: "Ajman",
       image: "/cities/Ajman.png",
+      subTitle: "Ajman is good",
     },
     {
       cityId: 13236,
       cityName: "Abu Dhabi",
       image: "/cities/Abu-dhabi.jpeg",
+      subTitle: "Ajman is good",
     },
     {
       cityId: 13668,
       cityName: "Dubai City",
       image: "/cities/Dubai.jpeg",
+      subTitle: "Ajman is good",
     },
     {
       cityId: 13765,
       cityName: "Fujairah",
       image: "/cities/Fujairah.jpeg",
+      subTitle: "Ajman is good",
     },
     {
       cityId: 14644,
       cityName: "Ras al Khaimah",
       image: "/cities/Ras-al-khaimah.jpeg",
+      subTitle: "Ajman is good",
     },
     {
       cityId: 14777,
       cityName: "Sharjah",
       image: "/cities/Sharja.jpeg",
+      subTitle: "Ajman is good",
     },
   ]);
 
