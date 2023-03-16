9201833bd85b8f367995c4580e1cadbbe234060e
Harikrishnan <harikrishnan@Harikrishnans-MacBook-Air.local>
2023-03-16 22:51:03 +0530

    gradient added

--- components/card/Image.vue
+++ components/card/Image.vue
@@ -6,10 +6,12 @@
       class="rounded-lg w-full h-full bg-cover"
     />
 
-    <div class="absolute inset-0 text-white flex flex-col justify-end p-4">
+    <div class="absolute inset-0 text-white flex flex-col justify-end p-4 z-10">
       <h1 class="text-xl font-bold">{{ cityName }}</h1>
       <p class="text-sm">{{ subtitle }}</p>
     </div>
+
+    <div class="absolute inset-0 bg-gradient-to-r from-[#000000ab]"></div>
   </div>
 </template>
 
