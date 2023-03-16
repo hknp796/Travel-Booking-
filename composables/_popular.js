106e13ce8d8f9d887ab5dd8acb7c26f3623d3ce2
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-17 00:08:50 +0530

    added top categories text

--- composables/_popular.js
+++ composables/_popular.js
@@ -5,6 +5,31 @@ const global = useGlobal();
 
 export const usePopular = defineStore("popular", () => {
   const tourStaticData = ref([]);
+
+  const topCategories = ref([
+    { id: 0, title: "Tours", image: "", alt: "Tour icon" },
+    { id: 1, title: "Tickets", image: "", alt: "Tickets icon" },
+    {
+      id: 2,
+      title: "Ariel Sightseeing",
+      image: "",
+      alt: "Ariel sightseeing icon",
+    },
+    { id: 3, title: "Cruises", image: "", alt: "cruises icon" },
+    { id: 4, title: "Connectivity", image: "", alt: "connectivity icon" },
+    { id: 5, title: "Adventure", image: "", alt: "adventure icon" },
+    { id: 6, title: "Water Activities", image: "", alt: "water activity icon" },
+    {
+      id: 7,
+      title: "Nature & Wildlife",
+      image: "",
+      alt: "nature & wildlife icon",
+    },
+    { id: 8, title: "Wellness", image: "", alt: "wellness icon" },
+    { id: 9, title: "Food & Drinks", image: "", alt: "food and drinks icon" },
+    { id: 10, title: "Transportation", image: "", alt: "transportation icon" },
+  ]);
+
   async function fetchTourStaticData(config, info) {
     /**
      * Expects: countryId and cityId
@@ -33,5 +58,5 @@ export const usePopular = defineStore("popular", () => {
     }
   }
 
-  return { fetchTourStaticData, tourStaticData };
+  return { fetchTourStaticData, tourStaticData, topCategories };
 });
