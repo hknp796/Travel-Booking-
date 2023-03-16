69ffc7a010d5db1577bdacdbbab3618ace614311
Harikrishnan <harikrishnan@Harikrishnans-MacBook-Air.local>
2023-03-17 00:33:34 +0530

    left scroll button added

--- components/card/OutlinedIcon.vue
+++ components/card/OutlinedIcon.vue
@@ -1,7 +1,19 @@
 <template>
   <div
-    class="min-w-[200px] h-[200px] rounded-lg p-10 border-2 border-black"
-  ></div>
+    class="min-w-[200px] h-[200px] rounded-lg p-10 border-2 border-black cursor-pointer"
+  >
+    <div class="flex flex-col justify-center items-center">
+      <img :src="image" alt="" />
+
+      <p class="text-sm font-medium text-gray-700">{{ title }}</p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  title: String,
+  image: String,
+  alt: String,
+});
+</script>
--- components/sections/Categories.vue
+++ components/sections/Categories.vue
@@ -5,14 +5,21 @@
       class="flex overflow-x-scroll mt-4 gap-5 lg:w-full"
       ref="scrollSection"
     >
-      <CardOutlinedIcon v-for="index in 6" :key="index" />
+      <CardOutlinedIcon v-for="index in 16" :key="index" v-bind="dummy" />
 
       <button
         type="button"
-        class="text-black bg-white border hover:bg-gray-200 font-medium rounded-full text-sm p-2.5 text-center items-center absolute -right-3 top-[200px] hidden lg:block"
-        @click="scrolling"
+        class="text-black bg-white border hover:bg-gray-200 font-medium rounded-full text-sm p-2.5 text-center items-center absolute -right-3 top-[55%] hidden lg:block"
+        @click="scrollingRight"
       >
         <Icon name="mdi-light:arrow-right" width="30" height="30" />
+      </button>
+      <button
+        type="button"
+        class="text-black bg-white border hover:bg-gray-200 font-medium rounded-full text-sm p-2.5 text-center items-center absolute -left-3 top-[55%] hidden lg:block"
+        @click="scrollingLeft"
+      >
+        <Icon name="mdi-light:arrow-left" width="30" height="30" />
       </button>
     </div>
   </div>
@@ -20,10 +27,22 @@
 
 <script setup>
 const scrollSection = ref(null);
-function scrolling() {
+function scrollingRight() {
   scrollSection.value.scrollBy({
     left: 900,
     behavior: "smooth",
   });
 }
+function scrollingLeft() {
+  scrollSection.value.scrollBy({
+    left: -900,
+    behavior: "smooth",
+  });
+}
+
+const dummy = {
+  image: "/svg/ticket.svg",
+  alt: "",
+  title: "Tickets",
+};
 </script>
